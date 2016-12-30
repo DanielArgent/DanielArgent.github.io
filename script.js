1feafd8f437e431743984a07419c38b120dd0b89
DanielArgent <dtsvikevich@mail.ru>
2016-12-30 22:16:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
-$(
-    function(){
+$(function()
+{
     var hei = (333 * document.documentElement.clientWidth) / 1349;
-    var elem = $('nav');
-    var top = $(this).scrollTop();
-        if(top > hei){
-            elem.css('top', 0);
-        }    
+    var elem = $('#nav');var top = $(this).scrollTop();
+    if(top > hei) 
+        elem.css('top', 0);
+    else 
+        elem.css('top', ((333 * document.documentElement.clientWidth) / 1349) - top);
 
     $(window).resize(function() {
-        var elem = $('nav');
+        var elem = $('#nav');
         var top = $(this).scrollTop();
-   
-            if(top > (333 * document.documentElement.clientWidth) / 1349){
-                elem.css('top', 0);
-            } else {
-                elem.css('top', (333 * document.documentElement.clientWidth) / 1349);
-            }
-    });         
-     
+        if(top > (333 * document.documentElement.clientWidth) / 1349)
+            elem.css('top', 0);
+        else 
+            elem.css('top', ((333 * document.documentElement.clientWidth) / 1349) - top);
+    });    
+
     $(window).scroll(function(){
         var hei = (333 * document.documentElement.clientWidth) / 1349;
         top = $(this).scrollTop();
-       
-            if (top < hei) {
-                elem.css('top', (hei - top));
-            } else {
-                elem.css('top', 0);
-    
-        }
+        if (top < hei) 
+            elem.css('top', (hei - top));
+        else 
+            elem.css('top', 0);
     });
 });
